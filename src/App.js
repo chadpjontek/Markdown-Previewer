136ee5889831c3bb771fd3abd51d95720ce52f6f
Chad Pjontek <chadpjontek@gmail.com>
2018-01-07 01:28:51 -0500

    Added marked is finished app component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const marked = require('marked');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    }
+  }
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Markdown Previewer</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <div className="markInput">
+            <h2>Input</h2>
+            <textarea onChange={this.handleChange} value={this.state.input} />
+          </div>
+          <div className="markOutput">
+            <h2>Output</h2>
+            <div dangerouslySetInnerHTML={{ __html: marked(this.state.input) }}></div>
+          </div>
+        </main>
+        <footer>
+          Coded by Chad Pjontek - 2018
+          <img src={logo} className="App-logo" alt="logo" />
+        </footer>
       </div>
-    );
+    )
   }
 }
 
